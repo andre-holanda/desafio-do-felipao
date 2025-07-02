55dac2af445d7038a4a71125042827f182ec02d4
andre-holanda <aholandac.programador@gmail.com>
2025-07-01 21:34:35 -0300

    feat: conclusão do segundo desafio de codigo do felipao -calculadora de partida rankeadas-

--- Desafio 2/desafioFelipao2.py
+++ Desafio 2/desafioFelipao2.py
@@ -1,11 +1,24 @@
 """ Desenvolvedor: André Holanda
     Data: 01/07/2025
     - Calculadora de partidas Rankeadas -
-    Resumo: Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador, depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+    
+    Resumo:
+        Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador, depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+        Se vitórias for menor do que 10 = Ferro
+        Se vitórias for entre 11 e 20 = Bronze
+        Se vitórias for entre 21 e 50 = Prata
+        Se vitórias for entre 51 e 80 = Ouro
+        Se vitórias for entre 81 e 90 = Diamante
+        Se vitórias for entre 91 e 100= Lendário
+        Se vitórias for maior ou igual a 101 = Imortal
+
+    ## Saída
+        Ao final deve se exibir uma mensagem:
+        "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
 """
 
-def menu():
-
+def menu():    
     menu = """
     ### Opções ###
 
@@ -14,8 +27,35 @@ def menu():
 
     Informe o número:
 """
+    return int(input(f"{menu}    -> "))
 
-    return int(input(f"{menu} -> "))
+def calcularVitorias(vitorias, derrotas):
+    resultado = vitorias - derrotas
+    if resultado < 0:
+        resultado = 0
+
+    rank = ranking(resultado)
+
+    return resultado, rank
+
+def ranking(pontos):
+    match pontos:
+        case _ if pontos <= 10:
+            ranking = "Ferro"
+        case _ if 11 <= pontos <= 20:
+            ranking = "Bronze"
+        case _ if 21 <= pontos <= 50:
+            ranking = "Prata"
+        case _ if 51 <= pontos <= 80:
+            ranking = "Ouro"
+        case _ if 81 <= pontos <= 90:
+            ranking = "Diamante"
+        case _ if 91 <= pontos <= 100:
+            ranking = "Lendário"
+        case _:
+            ranking = "Imortal"
+        
+    return ranking
 
 def main():
 
@@ -25,6 +65,13 @@ def main():
         match opcao:
             case 1:
                 print("\nJogo iniciado")
+                jogador = input("Informe o nome do seu Personagem: ")
+                vitorias = int(input("Informe a quantidade de Vitórias: "))
+                derrotas = int(input("Informe a quantidade de Derrotas: "))
+
+                resultado, ranking = calcularVitorias(vitorias, derrotas)
+
+                print(f"O Herói {jogador}, tem saldo de {resultado}, e está no nível de {ranking} ")
             case 2:
                 print("\nEncerrando jogo")
                 break
@@ -32,45 +79,3 @@ def main():
                 print("\nEscolha uma opção válida !")
 
 main()
-
-#     jogador = input("Informe o nome do seu Personagem: ")
-#     vitorias = int(input("Informe a quantidade de Vitórias: "))
-#     derrotas = int(input("Informe a quantidade de Derrotas: "))
-
-    
-
-# while True:
-    
-#     if xp <= 1_000:
-#         nivel = "Ferro"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     elif (xp >= 1_001) and (xp <= 2_000):
-#         nivel = "Bronza"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     elif (xp >= 2_001) and (xp <= 5_000):
-#         nivel = "Prata"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     elif (xp >= 6_001) and (xp <= 7_000):
-#         nivel = "Ouro"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     elif (xp >= 7_001) and (xp <= 8_000):
-#         nivel = "Platina"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     elif (xp >= 8_001) and (xp <= 9_000):
-#         nivel = "Ascendente"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     elif (xp >= 9_001) and (xp <= 10_000):
-#         nivel = "Imortal"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-#     else:
-#         nivel = "Radiante"
-#         print(f"O Herói de nome ** {nome} ** está no nível de ** {nivel} **")
-    
-#     nova_execucao = input("Deseja executar novamente (s ou n): ")
-#     if nova_execucao.lower() == "s":
-#         continue
-#     elif nova_execucao.lower() == "n":
-#         break
-#     else:
-#         print("Vocês escolheu uma opção inválida, execução encerrada!")
-#         break
